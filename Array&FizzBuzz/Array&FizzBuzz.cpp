9008d88ef6cab543b3b5c60088e5ad3f2ed32ce6
platingsquare <alexmorrow2002@gmail.com>
2025-10-07 15:34:42 +0200

    10/7/25 lektion

--- Array&FizzBuzz/Array&FizzBuzz.cpp
+++ Array&FizzBuzz/Array&FizzBuzz.cpp
@@ -18,7 +18,7 @@ void option1() { //Prime Number Checker
         cout << "This is the Prime Number Checker!\n";
         cout << "Write any number and I will tell you if it's a prime number\n";
     }
-	cout << "Do you want to check another number? (1=yes 0=no): ";
+	cout << "Do you want to check another number? (1=yes 0=no):\n";
 	cin >> again;
 }
 void option2() {
@@ -48,7 +48,7 @@ void option2() {
 void option3() {
 	// Function for option 3: FizzBuzz
     cout << "\n[FizzBuzz]\n";
-    for (int i = 1; i <= 20; ++i) {
+    for (int i = 1; i <= 100; ++i) {
         if (i % 15 == 0) cout << "FizzBuzz\n";
         else if (i % 3 == 0) cout << "Fizz\n";
         else if (i % 5 == 0) cout << "Buzz\n";
@@ -73,12 +73,12 @@ void option5() {
         cout << "Enter temperature: ";
         cin >> temp;
         switch (choice) {
-        case 1: result = (temp * 9 / 5) + 32; cout << result << " °F\n"; break;
-        case 2: result = (temp - 32) * 5 / 9; cout << result << " °C\n"; break;
-        case 3: result = temp + 273.15; cout << result << " K\n"; break;
-        case 4: result = temp - 273.15; cout << result << " °C\n"; break;
-        case 5: result = (temp - 32) * 5 / 9 + 273.15; cout << result << " K\n"; break;
-        case 6: result = (temp - 273.15) * 9 / 5 + 32; cout << result << " °F\n"; break;
+        case 1: result = (temp * 9 / 5) + 32; cout << result << " °F\n"; break;             // Celsius to Fahrenheit 
+        case 2: result = (temp - 32) * 5 / 9; cout << result << " °C\n"; break;             // Fahrenheit to Celcius
+        case 3: result = temp + 273.15; cout << result << " K\n"; break;                    // Celcius to Kelvin  
+        case 4: result = temp - 273.15; cout << result << " °C\n"; break;                   // Kelvin to Celcius
+        case 5: result = (temp - 32) * 5 / 9 + 273.15; cout << result << " K\n"; break;     // Fahrenheit to Kelvin
+        case 6: result = (temp - 273.15) * 9 / 5 + 32; cout << result << " °F\n"; break;    // Kelvin to Fahrenheit
         default: cout << "Invalid choice.\n";
 			// If it's below 5°C || 41°F || 278K, print "It's getting cold now"
 			// if it's below 0°C || 32°F || 273K, print "It's freezing"
@@ -93,21 +93,37 @@ void option5() {
 			// if it's above 100°C || 212°F || 373K, print "It's boiling now"
         }
         cout << "Another conversion? (1=yes 0=no): ";
-        cin >> again;
+        std::cin >> again;
     }
 }
 
 
 void option6() { //Databehandling 
-  //  int number = std:: 101;
+    cout << "Calculator build in progress"; {
+    }//  int number = std:: 101;
+        /* if (choice > 8)
+        cout << "Welcome to our local Calculator, what do you need?";
+        cout << "1) Basic Algebra\n2) Median Calculation\n3) Varians\n4) Max of Three\n5) Pythagorean Theory\n6) Graph / Statistics";
+        switch (choice) {
+        case 1:
+        case 2: float average(float a, float b)
+        case 3:ex 3 + 4 + 5 = 12, t = 12 12 / 3 4, 4 = m, (3 - 4) ^ 2 =
+        case 4: (float a, float b, float c)
+        case 5: a^2 + b^2 = c^2 || a*a + b*b = c*c sqrt a^2 = a
+        case 6: add numbers, when done they will be shown in visuell representation
+        */
 }
+
+    
+
 
 void option7() { //Array
-    std::cout << "\n";
+        cout << "\n";
 }
 
+
 void option8() { //Exit
-    std::cout << "\nGoodbye!\n";
+    cout << "\nGoodbye!\n";
 }
 
 // ----------- Main program -----------
@@ -122,7 +138,7 @@ int main() {
             << "2) Random Number Guessing Game\n" //almost done
             << "3) FizzBuzz\n" //Done
             << "4) Debugging\n"
-            << "5) Temperature\n"// I think it's done
+            << "5) Temperature\n"//almost done
             << "6) --Data-- TBD\n"
 			<< "7) Array - TBD\n"
             << "8) Exit\n" //Done
@@ -139,7 +155,7 @@ int main() {
         case 6: option6(); break;
 		case 7: option7(); break;
         case 8: option8(); playing = false; break;
-        default: cout << "Invalid choice.\n"; break;
+        default: cout << "Invalid choice.\n"; break; //add if playing = false insert cout << "A random qoute or fact"
         }
     }
     return 0;
@@ -159,3 +175,26 @@ double avg = sum / 5.0;*/
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+/* void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = *temp;
+}
+int x = 3, y = 4;
+cout << "Before swap: x=" << x << ", y=" << y << std::endl;
+swap(&x,&y);
+cout << "After swap: x=" << x << ", y=" << y << std::endl;
+void setLedState(bool* led, bool state) {
+    if (led) {
+        *led = state;
+        * 
+        * 
+        * bool led = false;
+        * setLedState(&led, true);
+        * cout << "LED state:" << (led ? "On" : "Off");
+        * 
+        * Add Calculator for next time
+        * Maybe try going for chess? making a chess bot? Q-Queen,K-King,B-Bishop,Kn-Knight,R-Rook,P-Pawn. int for how Q,K,B,Kn,R and P can move on a 8x8 board
+*/
